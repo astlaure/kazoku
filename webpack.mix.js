c6057c1c6a7f2b06e9efd868e0337375ebd7e481
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2022-02-09 09:02:16 -0500

    assets setup 🎨

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix
+    .disableSuccessNotifications()
+    .js('resources/js/app.js', 'public/js').version()
+    .sass('resources/css/app.scss', 'public/css', [
         //
-    ]);
+    ]).version()
+    .browserSync(process.env.APP_URL);
